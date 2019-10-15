1b80590fb8d5f3f0ea09fcde8da8c8fd2f6d4689
Allan Crisostomo <allan.crisostomo@whitecloak.com>
2019-10-15 10:50:16 +0800

    Fix response

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('di pre')
+  res.send('<style>html{width:100%;height:100%;display:grid;place-content:center;font-size:20vmin}body{width:100%;height:100%}</style><body>di pre</body>')
 });
 
 module.exports = router;
